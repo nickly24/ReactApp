4bdd9ade8e2ab871b5a539507046868280a8929f
nickly24 <nikolaypribysh@mail.ru>
2024-11-30 12:06:03 +0300

    spisok

--- src/Com.js
+++ src/Com.js
@@ -1,6 +1,14 @@
 
 export default function(){
+    const data = [
+        {id: 1, name: 'John', age: 25},
+        {id: 2, name: 'Jane', age: 30},
+        {id: 3, name: 'Bob', age: 35}
+    ]
     return (
-        <div>My comp</div>
+        <div>{data.map((el) => (
+            <div key={el.id}>{el.name} - {el.age}</div>
+        ))}
+        </div>
     )
 }
